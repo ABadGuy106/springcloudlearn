4e79e0f049d7184527b2f39daacb25e77532fcde
liujiwei <liujiwei0529@163.com>
2018-08-05 19:31:10 +0800

    项目创建

---
+++ a-simple-provider-user/src/main/java/net/abadguy/asimpleprovideruser/ASimpleProviderUserApplication.java
@@ -0,0 +1,12 @@
+package net.abadguy.asimpleprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASimpleProviderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ASimpleProviderUserApplication.class, args);
+    }
+}
---
+++ b-simple-consumer-movie/src/main/java/net/abadguy/bsimpleconsumermovie/BSimpleConsumerMovieApplication.java
@@ -0,0 +1,12 @@
+package net.abadguy.bsimpleconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSimpleConsumerMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BSimpleConsumerMovieApplication.class, args);
+    }
+}

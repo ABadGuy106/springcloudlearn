f3184e6222f78da91cf8fc665b14efed63bc04ac
liujiwei <liujiwei0529@163.com>
2018-08-05 23:41:14 +0800

    服务消费之调用服务提供者

--- b-simple-consumer-movie/src/main/java/net/abadguy/bsimpleconsumermovie/BSimpleConsumerMovieApplication.java
+++ b-simple-consumer-movie/src/main/java/net/abadguy/bsimpleconsumermovie/BSimpleConsumerMovieApplication.java
@@ -2,9 +2,15 @@ package net.abadguy.bsimpleconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BSimpleConsumerMovieApplication {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BSimpleConsumerMovieApplication.class, args);

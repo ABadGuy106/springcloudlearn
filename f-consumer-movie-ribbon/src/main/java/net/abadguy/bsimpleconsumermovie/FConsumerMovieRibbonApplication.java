3fd795a89227bdb7332f4b20fca8e772cff7edae
liujiwei <liujiwei0529@163.com>
2018-08-06 17:33:13 +0800

    使用注册到eureka的消费者，调用服务者

---
+++ f-consumer-movie-ribbon/src/main/java/net/abadguy/bsimpleconsumermovie/FConsumerMovieRibbonApplication.java
@@ -0,0 +1,24 @@
+package net.abadguy.bsimpleconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FConsumerMovieRibbonApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @LoadBalanced
+
+    public static void main(String[] args) {
+        SpringApplication.run(FConsumerMovieRibbonApplication.class, args);
+    }
+}
---
+++ f-consumer-movie-ribbon/src/main/java/net/abadguy/bsimpleconsumermovie/controller/MovieController.java
@@ -0,0 +1,20 @@
+package net.abadguy.bsimpleconsumermovie.controller;
+
+import net.abadguy.bsimpleconsumermovie.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable int id){
+        return this.restTemplate.getForObject("http://c-provider-user/findById?id="+id,User.class);
+    }
+}

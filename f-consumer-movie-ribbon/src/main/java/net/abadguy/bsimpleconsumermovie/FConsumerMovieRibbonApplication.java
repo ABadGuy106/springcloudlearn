60408bf168ca940c27a3f43a45b0bedc57050960
liujiwei <liujiwei0529@163.com>
2018-08-07 18:28:55 +0800

    将Ribbon的轮询路由改为随机路由

--- f-consumer-movie-ribbon/src/main/java/net/abadguy/bsimpleconsumermovie/FConsumerMovieRibbonApplication.java
+++ f-consumer-movie-ribbon/src/main/java/net/abadguy/bsimpleconsumermovie/FConsumerMovieRibbonApplication.java
@@ -1,14 +1,17 @@
 package net.abadguy.bsimpleconsumermovie;
 
+import net.abadguy.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="c-provider-user",configuration= TestConfiguration.class)
 public class FConsumerMovieRibbonApplication {
     @Bean
     @LoadBalanced
